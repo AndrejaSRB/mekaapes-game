a7ee378aa001775b4fc10dc98d64d12f5c87504b
Andreja <andreja@torchlighttechnology.com>
2021-12-14 12:52:52 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// components
+import Landing from './components/Landing/Landing';
+// Styles
+import './styles/main.scss';
+
+const App = () => {
+  return (
+    <div>
+        <Landing />
+    </div>
+  );
+}
+
+export default App;
