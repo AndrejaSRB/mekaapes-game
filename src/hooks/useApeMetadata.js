365e747e95e949e0b4affb9693efe58bf1bad71d
AndrejaSRB <andreja.kojadinovic@gmail.com>
2022-01-16 20:01:22 +0100

    fixed metadata headers

--- src/hooks/useApeMetadata.js
+++ src/hooks/useApeMetadata.js
@@ -1,18 +1,25 @@
 import { useQuery } from "react-query";
 
 const MEKAGAME_AWS_METADATA = process.env.REACT_APP_MEKAGAME_AWS_METADATA;
+const ORIGIN = process.env.REACT_APP_ORIGIN;
 
 const fetchItem = async (ape) => {
   if (ape.id && !ape.placeholder) {
-    let customId = 2002000 + +ape.id - 1000000;
-    let response = await fetch(`${MEKAGAME_AWS_METADATA}/${customId}.json`);
+    let customId = 2002000 + +ape.id;
+    let response = await fetch(`${MEKAGAME_AWS_METADATA}/${customId}.json`, {
+      method: "GET",
+      headers: {
+        Origin: ORIGIN,
+      },
+    });
     return response.json();
   }
 };
 
 const useApeMetadata = (ape) => {
   return useQuery(["ape-metadata", ape], () => fetchItem(ape), {
-    enabled: ape && (ape.reward !== null || ape.reward !== undefined) ? true : false,
+    enabled:
+      ape && (ape.reward !== null || ape.reward !== undefined) ? true : false,
   });
 };
 
