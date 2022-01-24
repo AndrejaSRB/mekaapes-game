9790843e6bb2c464a148c9a84b1c8e7f469c0b5f
AndrejaSRB <andreja.kojadinovic@gmail.com>
2022-01-24 16:22:03 +0100

    Fixed merging whitepaper section

--- src/pages/Whitepaper/Tables/Merge/Table.js
+++ src/pages/Whitepaper/Tables/Merge/Table.js
@@ -6,28 +6,28 @@ const MergeTable = () => (
     <Headline>
       <div className="empty">Level</div>
       <div className="full">$OG tax proportion</div>
-      <div className="full">Background</div>
+      <div className="full">Gifted Mints proportion</div>
     </Headline>
     <Body>
       <Cell>
         <div className="smaller">MekaApe</div>
         <div className="bigger">1x</div>
-        <div className="bigger">Color Gradient</div>
+        <div className="bigger">0</div>
       </Cell>
       <Cell>
         <div className="smaller">Mega Meka M1</div>
         <div className="bigger">2x</div>
-        <div className="bigger">Planetary</div>
+        <div className="bigger">1x</div>
       </Cell>
       <Cell>
         <div className="smaller">Mega Meka M2</div>
         <div className="bigger">2.8x</div>
-        <div className="bigger">Stars</div>
+        <div className="bigger">1.4x</div>
       </Cell>
       <Cell>
         <div className="smaller">Mega Meka M3</div>
-        <div className="bigger">3.8x</div>
-        <div className="bigger">4th Dimension</div>
+        <div className="bigger">3.6x</div>
+        <div className="bigger">3.6x</div>
       </Cell>
     </Body>
   </Table>
